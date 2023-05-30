2bd928cd73b25950e776e2341f0e6424f23a64c7
darkwolfie37 <darkwolfie@darksmp.com>
2023-05-30 05:10:10 -0400

    Fixed Building Issues

--- src/main/java/com/darksmp/upgradesmpmod/UpgradesmpmodMod.java
+++ src/main/java/com/darksmp/upgradesmpmod/UpgradesmpmodMod.java
@@ -21,7 +21,6 @@ import com.darksmp.upgradesmpmod.init.UpgradesmpmodModTabs;
 import com.darksmp.upgradesmpmod.init.UpgradesmpmodModSounds;
 import com.darksmp.upgradesmpmod.init.UpgradesmpmodModProcedures;
 import com.darksmp.upgradesmpmod.init.UpgradesmpmodModPaintings;
-import com.darksmp.upgradesmpmod.init.UpgradesmpmodModMenus;
 import com.darksmp.upgradesmpmod.init.UpgradesmpmodModItems;
 import com.darksmp.upgradesmpmod.init.UpgradesmpmodModFeatures;
 import com.darksmp.upgradesmpmod.init.UpgradesmpmodModBlocks;
@@ -41,8 +40,6 @@ public class UpgradesmpmodMod implements ModInitializer {
 		UpgradesmpmodModFeatures.load();
 		UpgradesmpmodModPaintings.load();
 		UpgradesmpmodModProcedures.load();
-
-		UpgradesmpmodModMenus.load();
 
 		UpgradesmpmodModSounds.load();
 
